cdbc1d7984587d63d77d78771f86e1fdbc2807c8
xitongsys <xitongsys@gmail.com>
2021-02-04 08:57:25 +0800

    Create number_of_simple_path_in_graph.cpp

---
+++ problems/number_of_simple_path_in_graph.cpp
@@ -0,0 +1,35 @@
+/*
+ * Give a connected graph with n edges and n point.
+ * Give m queries [[u0,v0],[u1,v1],...,[um, vm]]
+ * return the number of simple path between [ui,vi].
+ *
+ * constraints:
+ *
+ * 1 < n < 1e5
+ * 1 <= m < 1e6
+*/
+
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <functional>
+using namespace std;
+
+vector<int> numberOfSimplePath(vector<vector<int>> edges, vector<vector<int>> queries){
+	int n = edges.size(), m = queries.size();
+}
+
+int main(){
+	return 0;
+}
