714fdeaefdac68362840162dd1ac593827af83a1
xitongsys <xitongsys@gmail.com>
2022-09-23 08:32:36 +0800

    Create bintree_index_from_zero.cc

---
+++ bin_tree/bintree_index_from_zero.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+class BinTree : vector<ll> {
+public:
+    ll c0 = 0;
+    explicit BinTree(ll k = 0) //默认初始化一个能保存k个元素的空树状数组
+    {
+        assign(k + 1, 0); //有效下标从0开始
+    }
+    int lowbit(ll k)
+    {
+        return k & -k;
+        //也可写作x&(x^(x–1))
+    }
+    ll sum(ll k)
+    {
+        ll res = _sum(k) + c0;
+        return res;
+    }
+    ll _sum(ll k) //求第1个元素到第n个元素的和
+    {
+        ll res = (k > 0 ? _sum(k - lowbit(k)) + (*this)[k] : 0);
+        return res;
+    }
+    ll last() //返回最后一个元素下标
+    {
+        return size() - 1;
+    }
+    void add(ll k, ll w)
+    {
+        if (k == 0) {
+            c0 += w;
+            return;
+        }
+        _add(k, w);
+    }
+    void _add(ll k, ll w) //为节点k加上w
+    {
+        if (k > last())
+            return;
+        (*this)[k] += w;
+        _add(k + lowbit(k), w);
+    }
+};
