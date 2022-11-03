d4c85ca5c1d8517334ebcd44b70000b15a8f93a5
xitongsys <xitongsys@gmail.com>
2022-11-03 10:46:20 +0800

    Update sol.rs

--- sample/sol.rs
+++ sample/sol.rs
@@ -1,45 +1,41 @@
-use std::{
-    io::{self, Read},
-    iter::Scan,
-};
-
-struct Scanner {
-    it: std::vec::IntoIter<String>,
-}
-
-impl Scanner {
-    fn new() -> Scanner {
-        let mut buf = String::new();
-        std::io::stdin().read_to_string(&mut buf);
-
-        return Scanner {
-            it: {
-                std::io::stdin().read_to_string(&mut buf).unwrap();
-                buf.split_whitespace().map(|x|x.to_string()).collect::<Vec<String>>().into_iter()
-            }
-        };
-    }
-
-    fn next<T: std::str::FromStr>(&mut self) -> T 
-    where
-    <T as std::str::FromStr>::Err: std::fmt::Debug,{
-        return self.it.next().unwrap().parse().unwrap()
-    }
-}
+#[allow(unused_imports)]
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Default, Debug)]
-struct Solution {
-}
+struct Solution {}
 
 impl Solution {
-    fn solve(&mut self) {
-    }
+    fn solve(&mut self) {}
+
     fn main(&mut self) {
-        let mut sc = Scanner::new();
+        let mut sc = io::Scanner::default();
     }
 }
 
 fn main() {
     let mut sol = Solution::default();
     sol.main();
+}
+
+mod io {
+    use std::io::{stdin, stdout, BufWriter, Write};
+    #[derive(Default)]
+    pub struct Scanner {
+        buffer: Vec<String>,
+    }
+    impl Scanner {
+        fn next<T: std::str::FromStr>(&mut self) -> T {
+            loop {
+                if let Some(token) = self.buffer.pop() {
+                    return token.parse().ok().expect("Failed parse");
+                }
+                let mut input = String::new();
+                stdin().read_line(&mut input).expect("Failed read");
+                self.buffer = input.split_whitespace().rev().map(String::from).collect();
+            }
+        }
+    }
 }
